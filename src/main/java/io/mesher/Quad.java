524ec3422425fb250b7a1fb5e8faf4f8d7046ece
dustContributor <hctf90@gmail.com>
2026-04-19 00:31:15 -0300

    - Should validate these, also added convenience area method

--- src/main/java/io/mesher/Quad.java
+++ src/main/java/io/mesher/Quad.java
@@ -1,7 +1,22 @@
 package io.mesher;
 
+import java.util.Objects;
+
 import org.joml.Vector3ic;
 
+import io.mesher.misc.OpsCheck;
+
 public record Quad(Vector3ic position, Axis forwardAxis, Axis sideAxis, int forwardSize, int sideSize, int value) {
-  // Empty
+  public Quad {
+    Objects.requireNonNull(position, "postition");
+    Objects.requireNonNull(forwardAxis, "forwardAxis");
+    Objects.requireNonNull(sideAxis, "sideAxis");
+    OpsCheck.aboveZero(forwardSize, "forwardSize");
+    OpsCheck.aboveZero(sideSize, "sideSize");
+    // value can be anything, don't check it
+  }
+
+  public final int area() {
+    return forwardSize * sideSize;
+  }
 }
